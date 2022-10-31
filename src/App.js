2deb23033fe03db91e357f0a91e4f3e06101d600
SulbaranDev <angeldv.sulbaran@gmail.com>
2022-10-31 18:36:02 +0100

    Header is ready

--- src/App.js
+++ src/App.js
@@ -7,11 +7,70 @@ const Container =styled.div`
 
 `;
 
+const Header= styled.div`
+  display:flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: black;
+  color: white;
+  align-items: center;
+  padding: 10px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+
+const AppName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const MovieImage = styled.img`
+  width: 48px;
+  height: 48px;
+  margin: 15px;
+`;
+
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  background-color: white;
+  border-radius: 6px;
+  margin-left: 20px;
+  width: 50%;
+  background-color: white;
+  align-items: center;
+`;
+
+const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+`
 
 function App() {
   return (
     <Container>
-      React Movie App
+      <Header>
+        <AppName>
+          <MovieImage src="/movie-icon.svg" />
+          React Movie App
+        </AppName>
+        <SearchBox>
+          <SearchIcon src="/search-icon.svg"/>
+          <SearchInput placeholder="Search Movie"/>
+        </SearchBox>
+      </Header>
     </Container>
   );
 }
